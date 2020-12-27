eca2b310fb52c716cabffc14a879fd3aeb5e951b
linkor <ladrenaline@mail.ru>
2020-12-27 19:50:43 +0300

    add some changes

--- photo/main/views.py
+++ photo/main/views.py
@@ -11,20 +11,20 @@ def servises(request):
 
 
 def goods(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/goods.html')
 
 
 def reklamma(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/reklamma.html')
 
 
 def souvenirs(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/souvenirs.html')
 
 
 def workshop(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/workshop.html')
 
 
 def contacts(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/contacts.html')
