82dec3d6d793845d90b8dc843b96708c6403fe2e
linkor <ladrenaline@mail.ru>
2021-01-04 21:31:47 +0300

    изменил формы заказа

--- src/main/forms.py
+++ src/main/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Order
 
+
 class OrderForm(forms.ModelForm):
     class Meta():
         model = Order
-        fields = ('photo_size', 'photo_type', 'img', 'phone')
+        fields = ("phone",)
--- src/main/models.py
+++ src/main/models.py
@@ -1,20 +1,42 @@
 from django.db import models
+import uuid
 
+
+def get_uuid():
+    uid = str(uuid.uuid4().fields[0])
+    return uid
 
 class Order(models.Model):
-    order_uidd = "uuid"
-    photo_size = models.CharField('Размер фото', max_length=10)
-    photo_type = models.BooleanField("Матовая?")
-    img = models.ImageField('Загрузка фото', upload_to='photos/')
-    phone = models.CharField('Телефон для связи', max_length=12, default="")
-
-    def tuple(self):
-        return self.order_uidd, self.phone
+    order_uidd = models.CharField('Идентификатор', default=get_uuid(), max_length=10)
+    is_done = models.BooleanField('Выполнен?', default=False)
+    phone = models.CharField('Телефон для связи', max_length=20)
 
     class Meta():
         verbose_name = "Заказы"
         verbose_name_plural = "Заказ"
 
-    objects = models.Manager()
+
+class Photo(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    photo_uuid = models.CharField('Идентификатор', default=get_uuid(), max_length=10)
+
+    STATUS_CHOICES = [
+        ('10x15','10x15'),
+        ('15x21','15x21'),
+        ('21x30', '21x30'),
+        ('30x40','30x40'),
+        ('30x45','30x45'),
+    ]
+
+    photo_size = models.CharField('Размер фото', max_length=10, choices=STATUS_CHOICES, default=STATUS_CHOICES[0])
+    photo_type = models.BooleanField("Матовая?")
+    img = models.ImageField('Загрузка фото', upload_to='photos/')
+    count = models.IntegerField('Кол-во', default=1)
+
+
+    class Meta():
+        verbose_name = "Фотографии"
+        verbose_name_plural = "Фотография"
+
     
 
